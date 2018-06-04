a2f10b5e6193bab8c092c504c43dd590fcc03a31
xiaoliangyu <xiaoliang.yu@xiaoliangyudeMacBook-Pro.local>
2018-06-04 17:58:13 +0800

    modify the real barrage

--- src/components/barrage/index.js
+++ src/components/barrage/index.js
@@ -1,98 +1,99 @@
-import './index.less'
 import React from 'react';
-import {findDOMNode} from 'react-dom';
-export default class Barrage extends React.Component{
+import initialSize from '../../utils/flexiable';
+//Mobile terminal adaptive
+initialSize(window, window['lib'] || (window['lib'] = {}));
+export default class Barra extends React.Component{
   constructor(props){
     super(props);
     this.myCanvas=React.createRef();
   }
   componentDidMount(){
     let _this=this;
-    let canvas=findDOMNode(_this.myCanvas.current);
-    let rect=canvas.getBoundingClientRect();
-    let width=rect.right-rect.left;
-    let height=rect.bottom-rect.top;
-    let ctx=canvas.getContext('2d');
-    ctx.font='20px Microsoft YaHei';
-    _this.width=width;
-    _this.height=height;
-    _this.ctx=ctx;
-    _this.barrageList=[];
-    requestAnimationFrame(_this.draw.bind(_this));
-    const textList = ['弹幕', '666', '233333333',
-      'javascript', 'html', 'css', '前端框架', 'Vue', 'React',
-      'Angular','测试弹幕效果'
-    ];
-    textList.forEach((t) => {
-        _this.shoot(t);
+    const {
+      barrageList,
+      color
+    }=_this.props;
+    let canvas=this.myCanvas.current;
+    let ctx=canvas.getContext("2d");
+    ctx.font = "10px Courier New";
+    let width=canvas.width;
+    //get color of all barrage
+    let colorArr=_this.getColor(color);
+    //get the initial left for all barrage
+    let numArrL=_this.getLeft();
+    //get the initial top for all barrage
+    let numArrT=_this.getTop();
+    // get speed of all barrage
+    let speedArr=_this.getSpeed();
+    _this.timer=setInterval(function(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.save();
+        for(let j=0;j<barrageList.length;j++){
+          numArrL[j]-=speedArr[j];
+          ctx.fillStyle = colorArr[j]
+          ctx.fillText(barrageList[j],numArrL[j],numArrT[j]);
+          if(numArrL[j]<=-width){
+            numArrL[j]=canvas.width;
+          }
+        }
+        ctx.restore();
+     },16.7);
+  }
+  getTop(){
+    let _this=this;
+    let {barrageList}=this.props;
+    let canvas=_this.myCanvas.current;
+    let height=canvas.height;
+    let len=barrageList.length;
+    let arr=new Array(len).fill(1);
+    return arr.map(function(){
+      let tagHeight=Math.random()*height;
+      if(tagHeight<0.2*height){
+        return 0.2*height
+      }
+      if(tagHeight>0.8*height){
+        return 0.8*height
+      }
+      console.log(tagHeight)
+      return tagHeight
     });
   }
-  // add barrage
-  shoot(value){
+  getLeft(){
     let _this=this;
-    let top=_this.getTop();
-    let color=_this.getColor();
-    let offset=_this.getOffset();
-    let barrageList=_this.barrageList;
-    let width=Math.ceil(_this.ctx.measureText(value).width);
-    let barrage={
-      value:value,
-      top:top,
-      left:_this.width,
-      color:color,
-      offset:offset,
-      width:width
-    }
-    barrageList.push(barrage);
+    let {barrageList}=_this.props;
+    let canvas=_this.myCanvas.current;
+    let width=canvas.width;
+    let len=barrageList.length;
+    return new Array(len).fill(width);
   }
-  //draw
-  draw(){
+  getColor(color){
     let _this=this;
-    let barrageList=_this.barrageList;
-    if(barrageList.length){
-      _this.ctx.clearRect(0,0,_this.width,_this.height);
-      for(let i=0;i<barrageList.length;i++){
-        let b=barrageList[i];
-        if(b.left+b.width<=0){
-          _this.shoot(barrageList[i].value);
-          barrageList.splice(i,1);
-          continue;
-        }
-        b.left-=b.offset;
-        _this.drawText(b);
-        _this.ctx.restore();
-      }
-
-    }
-    requestAnimationFrame(_this.draw.bind(_this));
+    let {barrageList}=_this.props;
+    let len=barrageList.length;
+    //random color
+    //the empty will skip，so fill 1 with the all array
+    let arr=new Array(len).fill(1);
+    return arr.map(function(){
+      return '#'+Math.floor(Math.random()*0xffffff).toString(16);
+    });
   }
-  //start draw
-  drawText(barrage){
+  getSpeed(){
     let _this=this;
-    let _ctx=_this.ctx;
-    _ctx.fillStyle=barrage.color;
-    _ctx.fillText(barrage.value,barrage.left,barrage.top);
-    // _ctx.strokeStyle=barrage.color;
-    // _ctx.strokeRect(barrage.left,barrage.top,barrage.width,-30);
+    let {barrageList}=_this.props;
+    let len=barrageList.length;
+    //random speed
+    let arr=new Array(len).fill(1);
+    return arr.map(function(){
+      return parseInt(Math.random()*5)
+    })
   }
-  //color
-  getColor(){
-    //random
-    return '#'+Math.floor(Math.random()*0xffffff).toString(16);
-  }
-  //be relative to the Top Left corner
-  getTop(){
-    return 30
-  }
-  // offset
-  getOffset(){
-    //random
-    return +(Math.random()*4).toFixed(1)+1;
+  componentWillUnmount(){
+    let _this=this;
+    clearTimeInterval(_this.timer);
   }
   render(){
     return <div className="m-barrage">
              <canvas className="m-barrage-canvas" ref={this.myCanvas}></canvas>
            </div>
   }
-
 }
